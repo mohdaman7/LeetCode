ecc51a72a890b39da07f18ffaac64616dc87490f
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2024-10-14 10:54:07 +0530

    3110. Score of a String

---
+++ ScoreOfAString.js
@@ -0,0 +1,11 @@
+let s = 'hello';
+
+var scoreOfString = function(s) {
+    let sum = 0
+    for(let i=0;i<s.length-1;i++){
+        sum += Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1))
+    }
+    return sum
+};
+
+console.log(scoreOfString(s))

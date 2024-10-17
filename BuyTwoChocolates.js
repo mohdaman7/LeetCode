f211a1d33d0bd4913571e6f4c9b68916cd267789
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2024-10-17 19:06:13 +0530

    2706. Buy Two Chocolates

---
+++ BuyTwoChocolates.js
@@ -0,0 +1,13 @@
+let prices = [1,2,2], money = 3
+
+var buyChoco = function(prices, money) {
+    prices.sort((a,b)=>a-b);
+    sum = prices[0] + prices[1];
+    if(sum<=money){
+        return money-sum;
+    }else{
+        return money
+    }
+};
+
+console.log(buyChoco(prices,money))

b4e1a634cfecdf9622601b8e19eb4762373a58a0
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2024-10-21 18:50:45 +0530

    Final Value of Variable After Performing Operations

---
+++ FinalValue.js
@@ -0,0 +1,15 @@
+let operations = ["--X","X++","X++"];
+
+var finalValueAfterOperations = function(operations) {
+    let x=0;
+    for(let i=0;i<operations.length;i++){
+        if(operations[i]=='--X'||operations[i]=='X--'){
+            x--;
+        }else{
+            x++
+        }
+    }
+    return x
+};
+
+console.log(finalValueAfterOperations(operations))

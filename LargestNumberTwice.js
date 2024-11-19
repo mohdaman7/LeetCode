365db2b8ef0a3f491d61c4ac08fb37eba74d66a1
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2024-11-19 17:56:14 +0530

    747. Largest Number At Least Twice of Others

---
+++ LargestNumberTwice.js
@@ -0,0 +1,19 @@
+//LargestNumberTwice
+
+let nums = [3,6,1,0];
+var dominantIndex = function(nums) {
+    let sort = [...nums]
+    sort = sort.sort((a,b)=>b-a);
+    let max = sort[0];
+    if(sort[1]*2>max){
+        return -1
+    }
+    for(let i=0;i<nums.length;i++){
+        if(max===nums[i]){
+            return i
+        }
+    }
+};
+
+console.log(dominantIndex(nums))
+

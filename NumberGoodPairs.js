e7f7ab0fe26d8a52018b8ee404dd740ed1da9850
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2024-11-22 14:55:53 +0530

    1512. Number of Good Pairs

---
+++ NumberGoodPairs.js
@@ -0,0 +1,15 @@
+
+let nums = [1,2,3,1,1,3]
+var numIdenticalPairs = function(nums) {
+    let count = 0;
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]==nums[j]){
+                count++
+            }
+        }
+    }
+    return count
+};
+
+console.log(numIdenticalPairs(nums))

5524a969ca68ba839b45ebfc1e0fa17ec39e703a
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2024-11-26 09:51:23 +0530

    2185. Counting Words With a Given Prefix

---
+++ CountingPrefix.js
@@ -0,0 +1,13 @@
+
+let words = ["pay","attention","practice","attend"], pref = "at"
+var prefixCount = function(words, pref) {
+    let count = 0;
+    for(let i=0;i<words.length;i++){
+        if(words[i].startsWith(pref)){
+            count++
+        }
+    }
+    return count
+};
+
+console.log(prefixCount(words,pref))

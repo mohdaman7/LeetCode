6396b685f4ba621f420a7685327ac8fd749e80c3
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2024-12-06 12:11:39 +0530

    2544. Alternating Digit Sum

---
+++ AlternatingDigit.js
@@ -0,0 +1,15 @@
+let n = 521;
+var alternateDigitSum = function(n) {
+    let arr = n.toString().split('');
+    let sum = 0;
+    for(let i=0;i<arr.length;i++){
+        if(i%2!=0){
+            sum += parseInt(-arr[i])
+        }else{
+            sum += parseInt(arr[i])
+        }
+    }
+    return sum
+};
+
+console.log(alternateDigitSum(n))

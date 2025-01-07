74a3f41c9bbcdf7932d018124ae6713b1bcb1aa5
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2025-01-07 16:34:24 +0530

    2027. Minimum Moves to Convert String

---
+++ MinimumMoves.js
@@ -0,0 +1,15 @@
+
+let s = "XXOX";
+
+var minimumMoves = function(s) {
+    let count = 0;
+    for(let i=0;i<s.length;i++){
+        if(s[i]=='X'){
+            count++
+            i+=2
+        }
+    }
+    return count
+};
+
+console.log(minimumMoves(s))

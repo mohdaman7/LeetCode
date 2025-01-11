04986cf49a64a9f5e901ca43c08da84436caf883
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2025-01-11 16:30:07 +0530

    1470. Shuffle the Array

---
+++ ShuffleTheArray.js
@@ -0,0 +1,17 @@
+
+let nums = [2,5,1,3,4,7], n = 3
+var shuffle = function(nums, n) {
+    let res = []
+    let j = 0;
+    for(let i=0;i<nums.length;i++){
+        if(i%2==0){
+            res.push(nums[j])
+            j++
+        }else{
+            res.push(nums[n])
+            n++
+        }
+    }
+    return res
+};
+console.log(shuffle(nums,n))

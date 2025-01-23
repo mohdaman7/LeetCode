bd3c6136ae656e14586d2f58c9c742f5cb35d34f
Muhammed Aman <muhammedaman2.oaman@gmail.com>
2025-01-23 19:29:43 +0530

    2367. Number of Arithmetic Triplets

---
+++ NumberOfArithmetic.js
@@ -0,0 +1,18 @@
+
+let nums = [0,1,4,6,7,10], diff = 3;
+
+var arithmeticTriplets = function(nums, diff) {
+    let count = 0;
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            for(let k=j+1;k<nums.length;k++){
+                if(nums[j]-nums[i]==diff&&nums[k]-nums[j]==diff){
+                    count++
+                }
+            }
+        }
+    }
+    return count
+};
+
+console.log(arithmeticTriplets(nums,diff))
